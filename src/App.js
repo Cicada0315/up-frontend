328e8bfec22583ed09ffaa3f922ce9b4fbb654f8
jean <jin.ahn.1027@gmail.com>
2021-06-27 16:45:29 -0400

    added about component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import About from './components/About';
 import Posts from './components/Posts'
 import Form from './components/Forms/PostForm';
 import Auth from './components/Forms/Auth'
@@ -33,6 +34,7 @@ const App = () => {
             <Route exact path="/posts/new" component={() => <Form currentPostId={currentPostId} setCurrentPostId={setCurrentPostId}/>} />
             <Route exact path="/posts/" component={() => <Posts currentPostId={currentPostId} setCurrentPostId={setCurrentPostId} setSubmitted={setSubmitted}/>} />
             <Route exact path="/posts/:currentPostId/edit" component={() => <Form currentPostId={currentPostId} setCurrentPostId={setCurrentPostId}/>} />
+            <Route path="/about" component={() => <About />} />
             <Route path="/" render={() => <Posts currentPostId={currentPostId} setCurrentPostId={setCurrentPostId} setSubmitted={setSubmitted}/>} />
             </Switch>
             </Router>
